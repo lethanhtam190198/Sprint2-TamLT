7318b20022771b15da6053ef7e10703497e126fd
lethanhtam190198 <tamle011998@gmail.com>
2022-11-07 09:35:56 +0700

    up

--- angular_book/book/src/app/app.module.ts
+++ angular_book/book/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
-import { LoginComponent } from './login/login.component';
-import { CartComponent } from './cart/cart.component';
-import { DetailComponent } from './detail/detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {BodyComponent} from './body/body.component';
+import {LoginComponent} from './login/login.component';
+import {CartComponent} from './cart/cart.component';
+import {DetailComponent} from './detail/detail.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ListBookComponent } from './list-book/list/list-book.component';
-import { CreateComponent } from './list-book/create/create.component';
-import { EditComponent } from './list-book/edit/edit.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ListBookComponent} from './list-book/list/list-book.component';
+import {CreateComponent} from './list-book/create/create.component';
+import {EditComponent} from './list-book/edit/edit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
@@ -24,10 +24,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {ToastrModule} from 'ngx-toastr';
 import {MatIconModule} from '@angular/material/icon';
-import { CategoryBookComponent } from './category-book/category-book.component';
-import { DetailCustomerComponent } from './detail-customer/detail-customer.component';
-import { CartDetailComponent } from './cart-detail/cart-detail.component';
-import { StatisticComponent } from './statistic/statistic.component';
+import {CategoryBookComponent} from './category-book/category-book.component';
+import {DetailCustomerComponent} from './detail-customer/detail-customer.component';
+import {CartDetailComponent} from './cart-detail/cart-detail.component';
+import {StatisticComponent} from './statistic/statistic.component';
+import {FacebookLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -46,27 +47,29 @@ import { StatisticComponent } from './statistic/statistic.component';
     CartDetailComponent,
     StatisticComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFirestoreModule,
-        MatFormFieldModule,
-        MatInputModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatSelectModule,
-        FormsModule,
-        ToastrModule.forRoot({
-            positionClass: 'toast-top-right',
-            progressBar: true,
-            tapToDismiss: true
-        }),
-        MatIconModule
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      progressBar: true,
+      tapToDismiss: true
+    }),
+    MatIconModule,
+  ],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
